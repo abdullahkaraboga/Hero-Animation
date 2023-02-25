c949ca9d3a730b142e374e80992f5e83b9ca323e
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-25 16:33:44 +0300

    Initial Commit

---
+++ HeroAnimation/HeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationApp.swift
+//  HeroAnimation
+//
+//  Created by Abdullah Karaboğa on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
